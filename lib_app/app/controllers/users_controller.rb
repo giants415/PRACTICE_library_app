6885717119b64d6bb2c417b2467e934a581f17d6
Andrew <andrew.vinocur@gmail.com>
2016-10-04 19:05:26 -0700

    adds create and show to routes and controller

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def show
+    user_id = params[:id]
+    @user = User.find_by_id(user_id)
+  end
+
+  def create
+    @user = User.create(user_params)
+    if user.save
+      redirect_to user_path(user)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
