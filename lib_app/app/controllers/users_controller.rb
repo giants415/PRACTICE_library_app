0baeb98d7b99a6b26311bde6c24a40fc7d0b2f56
Andrew <andrew.vinocur@gmail.com>
2016-10-04 20:47:53 -0700

    completes project, i forgot to git waaay earlier

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -13,10 +13,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if @user.save
-      redirect_to root_path
-      # redirect_to user_path(@user) USE AFTER SHOW TEMPLATE IS SET UP
-    end
+    login(@user)
+    redirect_to @user
   end
 
   private
