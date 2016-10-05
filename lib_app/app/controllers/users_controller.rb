88cf1c1f48d18ab7319e2d6c49b57d9fbb29bf3f
Andrew <andrew.vinocur@gmail.com>
2016-10-04 19:23:02 -0700

    enables bcrypt and modifies controller/model for user

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if user.save
-      redirect_to user_path(user)
+    if @user.save
+      redirect_to root_path
+      # redirect_to user_path(@user) USE AFTER SHOW TEMPLATE IS SET UP
     end
   end
 
