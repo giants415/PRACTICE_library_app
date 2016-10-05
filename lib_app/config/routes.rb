ebc0880c744d7be9fb04b03db0a22d0ce7d42665
Andrew <andrew.vinocur@gmail.com>
2016-10-05 11:32:08 -0700

    adds library_users#index route

--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/users/new", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
+  get "/users/:user_id/libraries", to: "library_users#index", as: "user_libraries"
 
   get "/login", to: "sessions#new"
   get "/logout", to: "sessions#destroy"
